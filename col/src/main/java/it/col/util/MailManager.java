ae9bfa6bd8525aa948c1cbfbe5c8aaeb99a64a52
gbetorre <giovanroberto.torre@univr.it>
2025-11-10 19:09:32 +0100

    Add method to send e-mail message with authentication on SMTP server | Implementato metodo per inviare e-mail tramite server SMTP pubblico

--- col/src/main/java/it/col/util/MailManager.java
+++ col/src/main/java/it/col/util/MailManager.java
@@ -1,9 +1,44 @@
-/**
- * 
+/*
+ *   Convenzioni On Line (COL-GeCo). 
+ *   Applicazione web: 
+ *   - per la visualizzazione delle convenzioni attivate dall'ateneo, 
+ *   - per la gestione delle convenzioni della pubblica amministrazione, 
+ *   - per ottenere notifiche riguardo le scadenze ed i rinnovi
+ *   - e per effettuare il monitoraggio delle attività legate alle convenzioni.
+ *
+ *   Agreements Mapping and Management Software (COL-GeCo).
+ *   Web application: 
+ *   - for viewing conventions activated by the university,
+ *   - for the management of public administration conventions,
+ *   - to obtain notifications regarding deadlines and renewals
+ *   - and to carry out monitoring of activities related to agreements..
+ *   
+ *   Copyright (C) 2025 Giovanroberto Torre
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, see <https://www.gnu.org/licenses/>.
+ *
+ *   Giovanroberto Torre <gianroberto.torre@gmail.com>
+ *   Universita' degli Studi di Verona
+ *   Via Dell'Artigliere, 8
+ *   37129 Verona (Italy)
  */
+
 package it.col.util;
 
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.Properties;
@@ -26,45 +61,54 @@ import javax.mail.internet.MimeMultipart;
 
 
 /**
+ * <p>This class contains methods for sending emails.</p> 
+ * 
  * @author trrgnr59
- *
+ * @author <a href="mailto:gianroberto.torre@gmail.com">Giovanroberto Torre</a>
  */
 public class MailManager {
 
-
+    
     /**
-     * Utility method to send simple HTML email
-     * @param session
-     * @param toEmail
-     * @param subject
-     * @param body
-     */
-    public static void sendEmail(Session session, String toEmail, String subject, String body) {
+     * <p>Sends an email message using the provided JavaMail Session.
+     * This method creates a MimeMessage using the given session, sets the recipient, subject,
+     * and body content, then sends the email via Transport.send(). 
+     * It assumes the session is properly configured 
+     * for SMTP authentication and connection.</p>
+     * 
+     * @param session the JavaMail Session configured with SMTP server details and authentication
+     * @param toEmail the recipient email address to which the message will be sent
+     * @param subject the subject line of the email message
+     * @param body the main content or body text of the email message
+    */
+    public static void sendEmail(Session session, 
+                                 String toEmail, 
+                                 String subject, 
+                                 String body) {
         try {
-          MimeMessage msg = new MimeMessage(session);
-          //set message headers
-          msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
-          msg.addHeader("format", "flowed");
-          msg.addHeader("Content-Transfer-Encoding", "8bit");
+            MimeMessage msg = new MimeMessage(session);
+            // Set message headers
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
 
-          msg.setFrom(new InternetAddress("gianroberto.torre@gmail.com", "Direttore Generale"));
+            msg.setFrom(new InternetAddress("gianroberto.torre@gmail.com", "Team Leader"));
 
-          msg.setReplyTo(InternetAddress.parse("no_reply@example.com", false));
+            msg.setReplyTo(InternetAddress.parse("no_reply@example.com", false));
 
-          msg.setSubject(subject, "UTF-8");
+            msg.setSubject(subject, "UTF-8");
 
-          msg.setText(body, "UTF-8");
+            msg.setText(body, "UTF-8");
 
-          msg.setSentDate(new Date());
+            msg.setSentDate(new Date());
 
-          msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
-          System.out.println("Message is ready");
-          Transport.send(msg);  
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+            System.out.println("Message is ready");
+            Transport.send(msg);
 
-          System.out.println("EMail Sent Successfully!!");
-        }
-        catch (Exception e) {
-          e.printStackTrace();
+            System.out.println("EMail Sent Successfully!!");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
     
@@ -123,14 +167,13 @@ public class MailManager {
     /**
 
      */
-    public static String sendEmail(String content) throws Exception {
-        //String mailTo = "albertomaria.arenaagostino@univr.it";
+    public static String sendEmail(String body) throws Exception {
         String mailTo = "lindamaria.frigo@univr.it, giovanni.olivieri@univr.it, elisa.puddu@univr.it, francesca.limberto@univr.it, giovanroberto.torre@univr.it";
         //String mailTo = "giovanroberto.torre@univr.it";
         InternetAddress[] addresses = InternetAddress.parse(mailTo);
-        String mailFrom = "giovanroberto.torre@univr.it";
+        String mailFrom = "convenzioniecentri@ateneo.univr.it";
         String subject = "Richiesta";
-        String mailContent = new String(content); 
+        String mailContent = new String(body); 
         Properties props = System.getProperties();  // Get system properties
         props.put("mailTo", mailTo);
         props.put("mail.smtp.host", "smtp.univr.it");
@@ -148,7 +191,55 @@ public class MailManager {
             message.setContent(mailContent, Constants.MIME_TYPE_HTML + "; charset=UTF-8");
             Transport.send(message);                    // Send message
         } catch (MessagingException mex) {
-            String msg = "Si e\' verificato un problema nel processamento del MimeMessage.\n" + mex.getMessage();
+            String msg = "Si e\' verificato un problema nel processamento del messaggio.\n" + mex.getMessage();
+            //log.warning(msg);
+            throw new Exception(msg, mex);
+        } catch (Exception ex) {
+            String msg = "Si e\' verificato un problema generico.\n" + ex.getMessage();
+            //log.warning(msg);
+            throw new Exception(msg, ex);
+        }
+        return "Email inviata";
+    }
+    
+    
+    /**
+
+     */
+    public static String sendEmail(String body, String username, String password) 
+            throws Exception {
+        //String mailTo = "albertomaria.arenaagostino@univr.it";
+        //String mailTo = "lindamaria.frigo@univr.it, giovanni.olivieri@univr.it, elisa.puddu@univr.it, francesca.limberto@univr.it, giovanroberto.torre@univr.it";
+        String mailTo = "giovanroberto.torre@univr.it";
+        InternetAddress[] addresses = InternetAddress.parse(mailTo);
+        String mailFrom = "giovanroberto.torre@univr.eu";
+        String subject = "Richiesta";
+        String mailContent = new String(body); 
+        Properties props = System.getProperties();  // Get system properties
+        props.put("mailTo", mailTo);
+        props.put("mail.smtp.host", "mail.smtp2go.com");
+        props.put("mail.smtp.port", "587"); // TLS port recommended
+        props.put("mail.smtp.auth", "true"); // SMTP2GO requires authentication
+        props.put("mail.smtp.starttls.enable", "true"); // Enable STARTTLS for encryption
+        props.put("mail.smtp.connectiontimeout", "10000"); // 10 seconds
+        props.put("mail.smtp.timeout", "10000"); // 10 seconds
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+                return new javax.mail.PasswordAuthentication(username, password);
+            }
+        });
+        
+        try {
+            MimeMessage message = new MimeMessage(session); // Define message
+            message.setFrom(new InternetAddress(mailFrom)); // Set the from address
+            //message.addRecipient(Message.RecipientType.TO, new InternetAddress(mailTo));    // Set the to address
+            message.setRecipients(Message.RecipientType.TO, addresses);
+            message.setSubject(subject, "UTF-8");
+            //message.setContent(mailContent, "text/html");   // Set the content
+            message.setContent(mailContent, Constants.MIME_TYPE_HTML + "; charset=UTF-8");
+            Transport.send(message);                    // Send message
+        } catch (MessagingException mex) {
+            String msg = "Si e\' verificato un problema nel processamento del messaggio.\n" + mex.getMessage();
             //log.warning(msg);
             throw new Exception(msg, mex);
         } catch (Exception ex) {
@@ -159,87 +250,5 @@ public class MailManager {
 
         return "Email inviata";
     }
-    
-    
-    /**
-
-     *
-    public static boolean sendEmail() throws Exception {
-        boolean sent = false;
-        //String mailTo = "albertomaria.arenaagostino@univr.it";
-        String mailTo = "giovanroberto.torre@univr.it";
-        String mailFrom = "giovanroberto.torre@univr.eu";
-        String subject = "Richiesta";
-        StringBuffer content = new StringBuffer("Egregio Dr., certe volte parlo con me stesso.");
-        content.append("<br><br>")
-               .append("ma sono così intelligente")
-               .append("<br><br>")
-               .append("che non mi capisco")
-               .append("<br><br>")
-               .append("Oscar Wilde")
-               .append("<br><br>")
-               .append("Distinti saluti")
-               .append("<br>")
-               .append("drg");
-        String mailContent = new String(content); 
-        Properties props = System.getProperties();  // Get system properties
-        props.put("mailTo", mailTo);
-        props.put("mail.smtp.host", "smtp.univr.it");
-        props.put("mail.smtp.port", "25");
-        props.put("mail.smtp.auth", "false");
-        props.put("mail.smtp.starttls.enable", "false");
-        Session session = Session.getInstance(props);   // Get session
-        try {
-            MimeMessage message = new MimeMessage(session); // Define message
-            message.setFrom(new InternetAddress(mailFrom)); // Set the from address
-            InternetAddress[] recipients = InternetAddress.parse(mailTo);
-            for (InternetAddress recipient : recipients) {
-              recipient.validate();
-            }
-            //message.addRecipient(Message.RecipientType.TO, new InternetAddress(mailTo));    // Set the to address
-            message.setRecipients(Message.RecipientType.TO, recipients);
-            message.setSubject(subject);
-            message.setContent(mailContent, "text/html");   // Set the content
-            Transport.send(message);                    // Send message
-            sent = true;
-        } catch (SendFailedException sfe) {
-            Address[] invalidAddresses = sfe.getInvalidAddresses();
-            if (invalidAddresses != null) {
-                String msg = "Invalid address: ";
-                for (Address addr : invalidAddresses) {
-                    msg += addr.toString() + " ";
-                }
-                throw new Exception(msg, sfe);
-            }
-            Address[] validUnsent = sfe.getValidUnsentAddresses();
-            if (validUnsent != null) {
-                String msg = "Valid unsent addresses:";
-                for (Address addr : validUnsent) {
-                    msg += addr.toString();
-                }
-                throw new Exception(msg, sfe);
-            }
-            Address[] validSent = sfe.getValidSentAddresses();
-            if (validSent != null) {
-                String msg = "Valid sent addresses:";
-                for (Address addr : validSent) {
-                    msg += addr.toString();
-                }
-                throw new Exception(msg, sfe);
-            }
-            throw new Exception(sfe);
-        } catch (MessagingException mex) {
-            String msg = "Si e\' verificato un problema nel processamento del MimeMessage.\n" + mex.getMessage();
-            //log.warning(msg);
-            throw new Exception(msg, mex);
-        } catch (Exception ex) {
-            String msg = "Si e\' verificato un problema generico.\n" + ex.getMessage();
-            //log.warning(msg);
-            throw new Exception(msg, ex);
-        }
-
-        return sent;
-    }*/
-
     
 }
